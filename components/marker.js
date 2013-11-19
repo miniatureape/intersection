a9047f724a40136376bf86cc80885820c0cfc3f4
Justin Donato <justin.donato@gmail.com>
2013-11-19 07:30:34 -0500

    Expand radius

--- components/marker.js
+++ components/marker.js
@@ -1,7 +1,7 @@
 (function() {
 
     var Marker = function(pos) {
-        this.size = new Tween(0, 30, 200);
+        this.size = new Tween(0, 0, 200);
         this.alpha = new Tween(0, .3, 200);
         this.pos = pos || new Vec(0, 0);
         this.rgb = [239,255,205];
@@ -26,17 +26,20 @@
         draw: function(ctx) {
             if (!this.visible) return;
             var fill = 'rgba(' + this.rgb.join(',') + ',' + this.alpha.val() + ')';
+            var stroke = 'rgb(153, 23, 60)';
             ctx.save();
             ctx.fillStyle = fill;
+            ctx.strokeStyle = stroke;
+            ctx.lineWidth = 2;
             ctx.translate(this.pos.x, this.pos.y);
             ctx.beginPath();
             ctx.arc(0, 0, this.size.val(), 0, Math.PI * 2);
             ctx.fill();
             ctx.beginPath();
-            ctx.moveTo(0, -(this.size.val() / 7));
-            ctx.lineTo(0, (this.size.val() / 7));
-            ctx.moveTo(-(this.size.val() / 7), 0);
-            ctx.lineTo((this.size.val() / 7), 0);
+            ctx.moveTo(0, -7);
+            ctx.lineTo(0, 7);
+            ctx.moveTo(-7, 0);
+            ctx.lineTo(7, 0);
             ctx.stroke();
             ctx.restore()
         }, 
